1eea86ee2ca322051c830d2098ad644c755c99e8
Maram Sayed <maramsayed2005@gmail.com>
2026-06-30 00:17:20 -0700

    Add README with project files and setup steps
    Added project structure and setup instructions.

---
+++ README.m
@@ -0,0 +1,40 @@
+Files
+stationery.db -> actual database
+server.js
+users-router.js
+shops-router.js
+products-router.js
+public
+    js -> script files
+        addProduct.js
+        addShop.js
+        addUser.js
+        like&Review.js
+        products.js
+        shops.js
+views
+        header.pug
+        addProduct.pug
+        addShop.pug
+        home.pug
+        product.pug
+        products.pug
+        shop.pug
+        shops.pug
+        user.pug
+        users.pug
+        register.pug
+
+
+Demonstration Video Link: 
+https://youtu.be/q8cX_BeSbHk
+
+
+To run the server file, follow these steps:
+1. open a terminal window in the project's directory
+2. enter "npm init"
+3. enter "npm install pug"
+4. enter "npm install express"
+5. enter "npm install body-parser"
+6. enter "npm install sqlite3"
+7. enter "node server.js"
